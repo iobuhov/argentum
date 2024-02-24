9028dd025d9392ddeedcba971a9ab561c46fd292
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-02-24 02:47:43 +0100

    chore: experiments and chores

--- widgets/checkbox-group/src/components/HelloWorldSample.tsx
+++ widgets/checkbox-group/src/components/HelloWorldSample.tsx
@@ -1,18 +1,17 @@
 import { ReactElement, createElement } from "react";
-import {Checkbox, CheckboxIndicator} from "@xxx/argentum-ui/checkbox";
+import { Checkbox, CheckboxIndicator } from "@xxx/argentum-ui/checkbox";
 
 export interface HelloWorldSampleProps {
     sampleText?: string;
 }
 
-
 export function HelloWorldSample({ sampleText }: HelloWorldSampleProps): ReactElement {
-    return <div className="widget-hello-world">
-        <Checkbox isIndeterminate>
-
-            <CheckboxIndicator />
-                           Hello {sampleText}
-
-        </Checkbox>
-    </div>;
+    return (
+        <div className="widget-hello-world">
+            <Checkbox isIndeterminate>
+                <CheckboxIndicator />
+                Hello {sampleText}
+            </Checkbox>
+        </div>
+    );
 }
