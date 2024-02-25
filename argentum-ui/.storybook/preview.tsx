7e30166d398992e632961714ddedc0ea24f35d15
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-02-25 13:47:35 +0100

    chore: update storybook preview

---
+++ argentum-ui/.storybook/preview.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { createElement } from "react";
+import type { Preview } from "@storybook/react";
+import "./preview.scss";
+
+const preview: Preview = {
+    globalTypes: {
+        colorSchema: {
+            description: "Global color schema",
+            defaultValue: "light",
+            toolbar: {
+                items: [
+                    {
+                        value: "light",
+                        icon: "circlehollow",
+                        title: "Light mode"
+                    },
+                    { value: "dark", icon: "circle", title: "Dark mode" }
+                ],
+                //Change title based on selected value
+                dynamicTitle: true
+            }
+        }
+    },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i
+            }
+        }
+    },
+
+    decorators: [
+        (Story, context) => {
+            const schema = context.globals.colorSchema as string;
+            return (
+                <div className="mx-page">
+                    <div className="mx-placeholder">
+                        <div
+                            className={`auk-viewport argentum-theme-radix ${schema === "dark" ? "dark" : "light"}`}
+                        >
+                            <Story />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    ]
+};
+
+export default preview;
